798ee28a2681e8e9054394ef0ca52db0600d9afe
Zidane Lineses <zdnjsf.temp@gmail.com>
2025-08-22 23:36:46 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        This is a test website
+      </h1>
+    </div>
+  );
+}
