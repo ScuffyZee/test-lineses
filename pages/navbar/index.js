c68b3fd6ea7e85ac3fd708a7a637bc2b5f94e5ce
Zidane Lineses <zdnjsf.temp@gmail.com>
2025-09-03 20:22:24 +0800

    commit4

--- pages/navbar/index.js
+++ pages/navbar/index.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header class="text-gray-600 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <Image src="/acheron.png" width={80} height={80} ></Image>
+    <Image src="/acheron.jpg" width={80} height={80} ></Image>
     <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
       <a class="mr-5 hover:text-gray-900">Home</a>
       <a class="mr-5 hover:text-gray-900">About Us</a>
