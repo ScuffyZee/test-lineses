575a33fc26a18aff1db5af0845639a043faf4be3
Zidane Lineses <zdnjsf.temp@gmail.com>
2025-09-03 20:13:17 +0800

    second commit

---
+++ pages/navbar/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+const Navbar = () => {
+  return (
+    <header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <Image src="/images.png" width={80} height={80} ></Image>
+    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900">Home</a>
+      <a class="mr-5 hover:text-gray-900">About Us</a>
+      <a class="mr-5 hover:text-gray-900">Contact Us</a>
+      <a class="mr-5 hover:text-gray-900">Pricing</a>
+    </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+</header>
+  )
+}
+
+export default Navbar
