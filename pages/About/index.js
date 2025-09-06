04a8f3cc89ef23d4d3524647666722f61c8263d9
Zidane Lineses <zdnjsf.temp@gmail.com>
2025-09-06 13:53:51 +0800

    commit9

--- pages/About/index.js
+++ pages/About/index.js
@@ -6,7 +6,7 @@ const About = () => {
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">About me</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Hi name is Zidane Josef R. Lineses I`&apos;`m a 3rd Year College Student Studying Computer Science, This website is one of my current projects that will also be a part of my portfolio. Computer Science has been my main interest early on, some of the mian factors that pushed me to study this major is my mother who is an IT graduate. </p>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Hi name is Zidane Josef R. Lineses I&apos;m a 3rd Year College Student Studying Computer Science, This website is one of my current projects that will also be a part of my portfolio. Computer Science has been my main interest early on, some of the mian factors that pushed me to study this major is my mother who is an IT graduate. </p>
     </div>
     
   </div>
